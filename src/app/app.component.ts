255aba7b756a1b1cfae4d3ad4981ee56dc7aa9ef
Erick Lui <erickjin.lui@gmail.com>
2017-06-14 22:58:56 -0700

    corrected display messages to only show one message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  powerlevel: number = 0;
   powerArr = this.createPowerLevel();
   createPowerLevel(){
     let arr: Array<number> = [];
--- src/app/super-saiyan-four/super-saiyan-four.component.ts
+++ src/app/super-saiyan-four/super-saiyan-four.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SuperSaiyanFourComponent implements OnInit {
   @Input() powerlevel;
+  boolArr: Array<boolean> = [false, false, false];
+  ngOnChanges(){
+    if (this.powerlevel === 50000) {
+      this.boolArr = [true, false, false];
+    }
+    else if (this.powerlevel > 20000) {
+      this.boolArr = [false, true, false];
+    }
+    else if (this.powerlevel > 9000) {
+      this.boolArr = [false, false, true];
+    }
+    return;
+  }
   constructor() { }
 
   ngOnInit() {
